3508264b2c6317c3f89d7c4e3f3506cd8288d743
shijiakun <shijiakun@grandage.cn>
2023-02-27 23:16:50 +0800

    细颗粒度->图文   编辑, 删除, 新增等功能完成

--- library/jobManage/sourceList.py
+++ library/jobManage/sourceList.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -27,36 +28,8 @@ class TestCase(unittest.TestCase):
         self.driver.get('http://146.56.203.127:8143/library-test/workbench/180010020230000068')
         time.sleep(2)
 
-    def test_account_manage(self):
+    def test_base_info(self):
         try:
-
-            # self.driver.find_element(By.XPATH, '//i[contains(@class, "el-icon-user")]').click()
-            # time.sleep(1)
-            # self.driver.find_element(By.XPATH, '//span[text()="资源列表"]').click()
-            # time.sleep(2)
-            # el_form_contents = self.driver.find_elements(By.XPATH, '//div[@class="el-form-item__content"]')
-            #
-            # jobName = el_form_contents[0].find_element(By.CLASS_NAME, 'el-input__inner')
-            # jobStatus = el_form_contents[1].find_element(By.CLASS_NAME, 'el-input__inner')
-            # jobStatus.click()
-            # time.sleep(1)
-            # jobStatusFinish = self.driver.find_element(By.XPATH,
-            #                                                '//li[@class="el-select-dropdown__item"]/span[text()="加工完成"]')
-            #
-            # jobStatusFinish.click()
-            # time.sleep(1)
-            #
-            # jobName.clear()
-            # jobName.send_keys('测试')
-            # self.driver.find_element(By.XPATH, '//span[text()="查询"]').click()
-            # time.sleep(2)
-            # el_rows = self.driver.find_elements(By.XPATH, '//tr[@class="el-table__row"]')
-            # for row in el_rows:
-            #     cols = row.find_elements(By.CLASS_NAME, 'cell')
-            #     if cols[1].text.lower() == '180010020230000069':
-            #         row.find_element(By.XPATH, '//span[text()="抽取"]').click()
-            # time.sleep(3)
-
             self.driver.find_element(By.XPATH, '//span[text()="编辑"]').click()
             time.sleep(1)
             sectionTitle = self.driver.find_element(By.XPATH, '//div[@class="section-title" and text() = " 工作区 "]')
@@ -116,7 +89,6 @@ class TestCase(unittest.TestCase):
             time.sleep(1)
             self.driver.find_element(By.XPATH, '//span[text()="同步"]').click()
             time.sleep(1)
-
             requiredFormItemList = baseInfo.find_elements(By.XPATH,
                                                           '//div[contains(@class, "el-form-item") and  contains(@class, "is-required")]')
 
@@ -141,119 +113,194 @@ class TestCase(unittest.TestCase):
                     currentValue = formItem.find_element(By.CLASS_NAME, 'el-input__inner').get_attribute('value')
                 self.assertEqual(labelValue[label], currentValue,
                                  '测试' + label.replace(':', '') + '表单编辑功能成功!')
+        except Exception as e:
+            logging.exception(e)
 
-            #
-            # self.driver.find_element(By.XPATH, '//span[text()="新增账户"]').click()
-            # time.sleep(1)
-            # dialog = self.driver.find_element(By.XPATH, '//div[@class="el-dialog" and @aria-label="新增账户"]')
-            # dialogBody = dialog.find_element(By.XPATH, '//div[@class="el-dialog__body"]')
-            # formInputs = dialogBody.find_elements(By.XPATH, '//input[@class="el-input__inner"]')
-            # timestamp = int(time.time())
-            # formInputs[7].send_keys('客户名称-测试' + str(timestamp))
-            # formInputs[8].send_keys('显示名称-测试' + str(timestamp))
-            # formInputs[9].click()
-            # time.sleep(1)
-            # firstOrgTreeNode = self.driver.find_elements(By.XPATH,
-            #                                              '//li[contains(@class, "el-select-dropdown__item")]//span[contains(text(), "测试部门5") and @class="el-tree-node__label"]')[
-            #     1]
-            #
-            # firstOrgTreeNode.click()
-            # formInputs[10].click()
-            # time.sleep(1)
-            # firstAccountStatusTreeNode = self.driver.find_elements(By.XPATH,
-            #                                                        '//li[contains(@class, "el-select-dropdown__item")]//span[text()="激活状态"]')[
-            #     1]
-            #
-            # firstAccountStatusTreeNode.click()
-            #
-            # formInputs[11].click()
-            # time.sleep(1)
-            # firstRoleTreeNode = self.driver.find_elements(By.XPATH,
-            #                                               '//li[contains(@class, "el-select-dropdown__item")]//span[text()="管理员"]')[
-            #     1]
-            #
-            # firstRoleTreeNode.click()
-            # time.sleep(1)
-            # self.driver.find_element(By.CSS_SELECTOR,
-            #                          '.el-dialog__footer:nth-child(3) .el-button--primary > span').click()
-            # time.sleep(2)
-            # accountName.clear()
-            # accountFullName.clear()
-            # accountName.send_keys('客户名称-测试' + str(timestamp))
-            # accountFullName.send_keys('显示名称-测试' + str(timestamp))
-            # self.driver.find_element(By.XPATH, '//span[text()="查询"]').click()
-            # time.sleep(2)
-            # el_rows = self.driver.find_elements(By.XPATH, '//tr[@class="el-table__row"]')
-            # self.assertEqual(1, len(el_rows), '根据新增参数查询记录为一条')
-            # cols = el_rows[0].find_elements(By.TAG_NAME, 'span')
-            # self.assertEqual('客户名称-测试' + str(timestamp), cols[0].text, '新增和查询记录的客户名称相同')
-            # self.assertEqual('显示名称-测试' + str(timestamp), cols[1].text, '新增和查询记录的显示名称相同')
-            # self.assertIn('测试部门5', cols[2].text, '新增和查询记录的所属机构相同')
-            # self.assertIn('管理员', cols[3].text, '新增和查询记录的所属角色相同')
-            # self.assertEqual('激活状态', cols[5].text, '新增和查询记录的账户状态相同')
-            #
-            # self.driver.find_element(By.XPATH, '//span[text()="编辑"]').click()
-            # time.sleep(1)
-            # dialog = self.driver.find_element(By.XPATH, '//div[@class="el-dialog" and @aria-label="编辑账户"]')
-            # dialogBody = dialog.find_element(By.XPATH, '//div[@class="el-dialog__body"]')
-            # formInputs = dialogBody.find_elements(By.XPATH, '//input[@class="el-input__inner"]')
-            #
-            # formInputs[13].clear()
-            # formInputs[13].send_keys('显示名称-测试编辑' + str(timestamp))
-            #
-            # formInputs[14].click()
-            # time.sleep(1)
-            # firstOrgTreeNode = self.driver.find_elements(By.XPATH,
-            #                                              '//li[contains(@class, "el-select-dropdown__item")]//span[contains(text(), "测试部门4") and @class="el-tree-node__label"]')[
-            #     2]
-            # firstOrgTreeNode.click()
-            # formInputs[15].click()
-            # time.sleep(1)
-            # firstAccountStatusTreeNode = self.driver.find_elements(By.XPATH,
-            #                                                        '//li[contains(@class, "el-select-dropdown__item")]//span[text()="锁定状态"]')[
-            #     2]
-            # firstAccountStatusTreeNode.click()
-            #
-            # self.driver.find_elements(By.XPATH, '//span[@class="el-input__suffix-inner"]')[9].click()
-            # time.sleep(1)
-            #
-            # firstRoleTreeNode = self.driver.find_elements(By.XPATH,
-            #                                               '//li[contains(@class, "el-select-dropdown__item")]//span[text()="hhhhhh"]')[
-            #     2]
-            # firstRoleTreeNode.click()
-            # time.sleep(1)
-            # saveButton = self.driver.find_elements(By.CSS_SELECTOR,
-            #                                        '.el-dialog__footer:nth-child(3) .el-button--primary > span')
-            # saveButton[1].click()
-            # time.sleep(2)
-            # accountName.clear()
-            # accountFullName.clear()
-            # accountName.send_keys('客户名称-测试' + str(timestamp))
-            # accountFullName.send_keys('显示名称-测试编辑' + str(timestamp))
-            # self.driver.find_element(By.XPATH, '//span[text()="查询"]').click()
-            # time.sleep(2)
-            # el_rows = self.driver.find_elements(By.XPATH, '//tr[@class="el-table__row"]')
-            # cols = el_rows[0].find_elements(By.TAG_NAME, 'span')
-            # self.assertEqual('客户名称-测试' + str(timestamp), cols[0].text, '编辑和查询记录的客户名称相同')
-            # self.assertEqual('显示名称-测试编辑' + str(timestamp), cols[1].text, '编辑和查询记录的显示名称相同')
-            # self.assertIn('测试部门4', cols[2].text, '编辑和查询记录的所属机构相同')
-            # self.assertIn('hhhhhh', cols[3].text, '编辑和查询记录的所属角色相同')
-            # self.assertEqual('锁定状态', cols[5].text, '编辑和查询记录的账户状态相同')
-            #
-            # self.driver.find_element(By.XPATH, '//span[text()="冻结"]').click()
-            # time.sleep(1)
-            # self.driver.find_element(By.XPATH, '//span[text()="查询"]').click()
-            # time.sleep(2)
-            # el_rows = self.driver.find_elements(By.XPATH, '//tr[@class="el-table__row"]')
-            # cols = el_rows[0].find_elements(By.TAG_NAME, 'span')
-            # self.assertEqual('注销状态', cols[5].text, '冻结功能正常')
-            #
-            # self.driver.find_element(By.XPATH, '//span[text()="删除"]').click()
-            # time.sleep(1)
-            # self.driver.find_element(By.CSS_SELECTOR, '.el-button--default:nth-child(2) > span:nth-child(1)').click()
-            # time.sleep(1)
-            # el_rows = self.driver.find_elements(By.XPATH, '//tr[@class="el-table__row"]')
-            # self.assertEqual(0, len(el_rows), '根据参数,删除按钮点击后查询记录为0')
+    def test_edit_graininess(self):
+        try:
+            self.driver.find_element(By.ID, 'tab-resource').click()
+            time.sleep(1)
+            self.driver.find_elements(By.XPATH, '//i[@class = "el-icon-edit"]')[0].click()
+            time.sleep(1)
+            sectionTitle = self.driver.find_element(By.XPATH, '//div[@class="section-title" and text() = " 工作区 "]')
+            graininess = sectionTitle.find_elements(By.XPATH, '//div[@class="workbench-content"]')[1]
+            requiredFormItemList = graininess.find_elements(By.XPATH,
+                                                            '//div[contains(@class, "el-form-item") and  contains(@class, "is-required")]')
+
+            labelValue = {}
+            for formIndex in range(12, len(requiredFormItemList)):
+                formItem = requiredFormItemList[formIndex]
+
+                label = formItem.find_element(By.CLASS_NAME, 'el-form-item__label').text
+                content = formItem.find_element(By.CSS_SELECTOR, '.el-form-item__content')
+
+                isSpan = False
+                try:
+                    content.find_element(By.CSS_SELECTOR, 'div:first-child')
+                except Exception as e:
+                    isSpan = True
+
+                if isSpan:
+                    continue
+
+                elInput = content.find_element(By.CSS_SELECTOR, 'div:first-child')
+                isSelect = True
+                try:
+                    elInput.find_element(By.CSS_SELECTOR, 'div:first-child')
+                except Exception as e:
+                    isSelect = False
+
+                isInput = True
+                try:
+                    elInput.find_element(By.CLASS_NAME, 'el-input__inner')
+                except Exception as e:
+                    isInput = False
+
+                isTextarea = True
+                try:
+                    elInput.find_element(By.CLASS_NAME, 'el-textarea__inner')
+                except Exception as e:
+                    isTextarea = False
+
+                if isSelect:
+                    self.driver.execute_script("arguments[0].scrollIntoView(false);", elInput)
+                    content.find_element(By.CLASS_NAME, 'el-icon-circle-close').click()
+                    time.sleep(1)
+                    elInput.find_element(By.CLASS_NAME, 'el-input__suffix-inner').click()
+                    time.sleep(1)
+                    selectOption = self.driver.find_elements(By.XPATH,
+                                                             '//div[contains(@class, "el-select-dropdown") and  contains(@class, "el-popper")]')
+                    selectItem = selectOption[8].find_elements(By.CSS_SELECTOR, '.el-select-dropdown__item')[0]
+                    selectItem.click()
+                    labelValue[label] = selectItem.find_element(By.TAG_NAME, 'span').text
+                    time.sleep(1)
+                elif isInput:
+                    input = elInput.find_element(By.CLASS_NAME, 'el-input__inner')
+                    input.clear()
+                    input.send_keys('测试' + label.replace(':', ''))
+                    labelValue[label] = '测试' + label.replace(':', '')
+                elif isTextarea:
+                    input = elInput.find_element(By.CLASS_NAME, 'el-textarea__inner')
+                    input.clear()
+                    input.send_keys('测试' + label.replace(':', ''))
+                    labelValue[label] = '测试' + label.replace(':', '')
+
+            saveButton = self.driver.find_element(By.XPATH, '//i[@class = "el-icon-document-add"]')
+            self.driver.execute_script("arguments[0].scrollIntoView(false);", saveButton)
+            saveButton.click()
+            time.sleep(3)
+            self.driver.find_element(By.XPATH, '//span[text()="同步"]').click()
+            time.sleep(1)
+
+            self.driver.find_elements(By.XPATH, '//i[@class = "el-icon-arrow-down"]')[0].click()
+            requiredFormItemList = graininess.find_elements(By.XPATH,
+                                                            '//div[contains(@class, "el-form-item") and  contains(@class, "is-required")]')
+            for formIndex in range(12, len(requiredFormItemList)):
+                formItem = requiredFormItemList[formIndex]
+                label = formItem.find_element(By.CLASS_NAME, 'el-form-item__label').text
+                content = formItem.find_element(By.CSS_SELECTOR, '.el-form-item__content')
+
+                isTextarea = True
+                try:
+                    formItem.find_element(By.CLASS_NAME, 'el-textarea__inner')
+                except Exception as e:
+                    isTextarea = False
+                isSelectTag = True
+                try:
+                    formItem.find_element(By.CLASS_NAME, 'el-select__tags')
+                except Exception as e:
+                    isSelectTag = False
+
+                isSpan = False
+                try:
+                    content.find_element(By.CSS_SELECTOR, 'div:first-child')
+                except Exception as e:
+                    isSpan = True
+
+                if isSpan:
+                    continue
+
+                currentValue = ''
+                if isTextarea:
+                    currentValue = formItem.find_element(By.CLASS_NAME, 'el-textarea__inner').get_attribute('value')
+                elif isSelectTag:
+                    currentValue = formItem.find_element(By.CLASS_NAME, 'el-select__tags-text').text
+                elif isSpan:
+                    currentValue = formItem.find_element(By.TAG_NAME, 'span').text
+                else:
+                    currentValue = formItem.find_element(By.CLASS_NAME, 'el-input__inner').get_attribute('value')
+                self.assertEqual(labelValue[label], currentValue,
+                                 '测试细粒度表单' + label.replace(':', '') + '表单编辑功能成功!')
+
+        except Exception as e:
+            logging.exception(e)
+
+    def test_graininess_remove(self):
+        try:
+            self.driver.find_element(By.ID, 'tab-resource').click()
+            time.sleep(1)
+            operationItems = self.driver.find_elements(By.XPATH,
+                                                       '//div[contains(@class, "form-section") and  contains(@class, "form-section-resource")]')
+
+            title = ''
+            startPage = ''
+            endPage = ''
+            for item in operationItems:
+                isRemoved = True
+                try:
+                    item.find_element(By.CLASS_NAME, 'el-icon-close')
+                except Exception as e:
+                    isRemoved = False
+                if not isRemoved:
+                    continue
+
+                item.find_element(By.CLASS_NAME, 'el-icon-arrow-down').click()
+                inputList = self.driver.find_elements(By.XPATH,
+                                                      '//div[contains(@class, "el-form-item") and not(contains(@class, "el-form-item__content"))]')
+                title = inputList[27].find_element(By.CLASS_NAME, 'el-form-item__content').find_element(By.TAG_NAME,
+                                                                                                        'span').text
+                startPage = inputList[28].find_element(By.CLASS_NAME, 'el-form-item__content').find_element(By.TAG_NAME,
+                                                                                                            'span').text
+                endPage = inputList[29].find_element(By.CLASS_NAME, 'el-form-item__content').find_element(By.TAG_NAME,
+                                                                                                          'span').text
+                item.find_element(By.CLASS_NAME, 'el-icon-close').click()
+                time.sleep(1)
+                confirmModal = self.driver.find_element(By.CLASS_NAME, 'el-message-box__btns')
+                confirmModal.find_elements(By.TAG_NAME, 'span')[1].click()
+                time.sleep(2)
+                try:
+                    message = self.driver.find_element(By.CLASS_NAME, 'el-message__content').text
+                except Exception as e:
+                    message = ''
+                self.assertEqual('删除成功！', message, '删除颗粒度记录成功')
+                break
+
+            self.driver.find_element(By.XPATH, '//span[text()="新增章节"]').click()
+            time.sleep(1)
+
+            inputList = self.driver.find_elements(By.XPATH,
+                                                  '//div[contains(@class, "el-form-item") and contains(@class, " is-required")]')
+            titleInput = inputList[12].find_element(By.CLASS_NAME, 'el-input__inner')
+            titleInput.clear()
+            titleInput.send_keys(title)
+            startPageInput = inputList[13].find_element(By.CLASS_NAME, 'el-input__inner')
+            startPageInput.clear()
+            startPageInput.send_keys(startPage)
+            endpageInput = inputList[14].find_element(By.CLASS_NAME, 'el-input__inner')
+            endpageInput.clear()
+            endpageInput.send_keys(endPage)
+            saveButton = self.driver.find_element(By.XPATH, '//i[@class = "el-icon-document-add"]')
+            self.driver.execute_script("arguments[0].scrollIntoView(false);", saveButton)
+            saveButton.click()
+            time.sleep(2)
+            try:
+                message = self.driver.find_element(By.CLASS_NAME, 'el-message__content').text
+            except Exception as e:
+                message = ''
+            self.assertEqual('保存成功', message, '新增颗粒度记录成功')
+
+            self.driver.find_element(By.XPATH, '//span[text()="同步"]').click()
+            time.sleep(1)
 
         except Exception as e:
             logging.exception(e)

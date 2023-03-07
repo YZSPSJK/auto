6d8eeee5be2b5c7a72cd205831aae24a93ce9655
shijiakun <shijiakun@grandage.cn>
2023-03-08 00:41:47 +0800

    新增文本区编辑功能

---
+++ library/jobManage/textarea.py
@@ -0,0 +1,63 @@
+import time
+
+import pyautogui as pyautogui
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import unittest
+import logging
+from BeautifulReport import BeautifulReport
+
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        chrome_options = Options()
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--window-size=1920,1080")
+        # s = Service('D:/python/python/chromedriver.exe')
+        s = Service('/Users/sjk/workspace/sjk/python/chromedriver')
+        self.driver = webdriver.Chrome(service=s, options=chrome_options)
+        self.driver.get('http://146.56.203.127:8143/library-test/home')
+        self.driver.add_cookie(
+            {'domain': '146.56.203.127', 'httpOnly': False, 'name': 'library_admin_token', 'path': '/', 'secure': False,
+             'value': 'Bearer%20eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiMTAwMyJdLCJ1c2VyX25hbWUiOiJ0ZXN0dXNlciIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJleHAiOjE2NzkwNjI3NjksImF1dGhvcml0aWVzIjpbIlJPTEVfUk9MRTAwMSIsIlJPTEVfQURNSU4iXSwianRpIjoiMzE1OWQwYjEtNDBjNy00MDliLTgwNWMtZDc4MTRjN2Q0ZjUyIiwiY2xpZW50X2lkIjoibWVzc2FnaW5nLWNsaWVudCIsImFkZGl0aW9uIjpudWxsfQ.OU1kgjeKnQwsKfqw9X7R-JNzGrGwZrhROhzvwGFsVqgI6lsEvZ2pvHPQm6KR8uscmNIkqqi6z9QTw7sVGdPrEPG2VSwnofgZJk7D8071ou4v-4V76oj36nZmSjnypnb4rI6cwU5yNMOIxlZUYhSciKCN_8UbW5MIRaRnt_U3ckQCXge4xYKLTf_ryYBGGhoqqO7ETnD-fitifuh-wHoIdMvKs0M2ZateZFjfsg8kQWUOHBoGvHH54ak_NwwAEipTixTEaouTzDEtfxyTStxcICA50D5ZQstMyRVU3yksvLje-7Gazqcu6Lo2QcHt4iBAw6KXoiiX9fQWu4bvDw_kBg'})
+        self.driver.refresh()
+        time.sleep(2)
+        self.driver.get('http://146.56.203.127:8143/library-test/workbench/180010020230000012')
+        time.sleep(2)
+
+    def test_edit_text(self):
+        try:
+            self.driver.find_elements(By.XPATH, '//i[@class = "el-icon-edit"]')[0].click()
+            time.sleep(1)
+
+            editTextAreaDiv = self.driver.find_element(By.XPATH,
+                                                       '//div[contains(@class, "section-wrapper-main") and  contains(@class, "edit-txt")]')
+            elTextArea = editTextAreaDiv.find_element(By.CLASS_NAME, 'el-textarea__inner')
+            editText = elTextArea.get_attribute('value')
+            elTextArea.clear()
+            elTextArea.send_keys('测试文本编辑功能')
+            self.driver.find_elements(By.XPATH, '//span[text()="保存"]')[1].click()
+            time.sleep(3)
+            firstFile = self.driver.find_element(By.ID, 'file1')
+
+            self.assertEqual(firstFile.text.replace(' ', ''), '测试文本编辑功能',
+                             '测试编辑文本区功能成功!')
+        except Exception as e:
+            logging.exception(e)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    suite.addTest(loader.loadTestsFromTestCase(TestCase))
+    br = BeautifulReport(suite)
+    br.report(filename='sourceList.html', description='测试报告',
+              report_dir='/Users/sjk/workspace/sjk/python/auto/testReport')
